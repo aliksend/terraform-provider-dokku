e4c1ea530d7edbfefa16f98bf4f87c66a5e837f9
Alik Send <aliknoname@gmail.com>
2024-11-02 18:57:06 -0500

    fix

--- provider/dokku_client/nginx_config.go
+++ provider/dokku_client/nginx_config.go
@@ -6,16 +6,16 @@ import (
 )
 
 func (c *Client) NginxConfigGetValue(ctx context.Context, appName string, property string) (string, error) {
-	stdout, _, err := c.RunQuiet(ctx, fmt.Sprintf("dokku nginx:report %s --nginx-%s", appName, property))
+	stdout, _, err := c.RunQuiet(ctx, fmt.Sprintf("nginx:report %s --nginx-%s", appName, property))
 	return stdout, err
 }
 
 func (c *Client) NginxConfigSetValue(ctx context.Context, appName string, property string, value string) error {
-	_, _, err := c.RunQuiet(ctx, fmt.Sprintf(`dokku nginx:set %s %s "%s"`, appName, property, value))
+	_, _, err := c.RunQuiet(ctx, fmt.Sprintf(`nginx:set %s %s "%s"`, appName, property, value))
 	return err
 }
 
 func (c *Client) NginxConfigResetValue(ctx context.Context, appName string, property string) error {
-	_, _, err := c.RunQuiet(ctx, fmt.Sprintf("dokku nginx:set %s %s", appName, property))
+	_, _, err := c.RunQuiet(ctx, fmt.Sprintf("nginx:set %s %s", appName, property))
 	return err
 }
